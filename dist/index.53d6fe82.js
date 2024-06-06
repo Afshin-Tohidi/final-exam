e83483f55824be4fe172d06c8b81b9cad16cd611
Afshin-Tohidi <tohidi.afshin11@gmail.com>
2024-06-06 23:17:49 +0330

    first commit

---
+++ dist/index.53d6fe82.js
@@ -0,0 +1,2 @@
+let e;const o=[],c=function(e,c,n){o.push({name:e,qty:c,price:n})};c("pizza",2,21e4),c("hamburger",1,18e4),console.log(o),console.log((e=0,o.forEach(o=>e+=+o.price*o.qty),e));
+//# sourceMappingURL=index.53d6fe82.js.map
